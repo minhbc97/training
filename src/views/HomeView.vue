fb46053e49078e086475f98f060dcb4b0067329d
MinhBui1212 <minhcongbui.bka60@gmail.com>
2023-11-09 22:04:45 +0700

    commit training code

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    
+    <span>Message: {{ msg }}</span>
+
+    <p>Using text interpolation: {{ rawHtml }}</p>
+    <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js + TypeScript App',
+      rawHtml: '<span style="color: red">This should be red.</span>'
+    }
+  },
+}
+</script>
